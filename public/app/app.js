a299fdc38a4caf1e67a5f49bff2d33c1b94b2969
johnjrobert <78804682+johnjrobert@users.noreply.github.com>
2021-12-12 20:57:53 -0500

    Initial commit

---
+++ public/app/app.js
@@ -0,0 +1,24 @@
+function route() {
+  let hashTag = window.location.hash;
+  let pageID = hashTag.replace("#/", "");
+  let homeID = "home";
+
+  if (pageID == "") {
+    pageID = homeID;
+  } else {
+    pageID = pageID;
+  }
+
+  MODEL.navToPage(pageID);
+}
+
+function initListeners() {
+  route();
+  $(window).on("hashchange", route);
+}
+
+//reads page, loads everything to be used.
+$(document).ready(() => {
+  console.log("loading...");
+  initListeners();
+});
