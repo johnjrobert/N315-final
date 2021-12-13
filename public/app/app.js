f8d3d90d3636aaa0e69ac46c7789ee1f15ccc1a4
johnjrobert <78804682+johnjrobert@users.noreply.github.com>
2021-12-12 22:31:40 -0500

    working json load

--- public/app/app.js
+++ public/app/app.js
@@ -17,8 +17,22 @@ function initListeners() {
   $(window).on("hashchange", route);
 }
 
+function loop() {
+  $.getJSON("data/data.json", (coffee) => {
+    $.each(coffee.COFFEEMAKERS, function (index, coffee) {
+      console.log("Recipe:" + coffee.name);
+      $("#coffeecont").append(
+        `<div id="insideCoffeeCont"><div class="coffee-image"><img src="${coffee.image}" alt=""></div><div class="coffee-info"><h1 class="coffee-name">${coffee.name}</h1><h1 class="coffee-price">Price:  ${coffee.price}</h1><div class="coffee-shipping-icon">${coffee.shippingicon}</div></div></div>`
+      );
+    });
+  }).fail((error) => {
+    console.log(error);
+  });
+}
+
 //reads page, loads everything to be used.
 $(document).ready(() => {
   console.log("loading...");
   initListeners();
+  loop();
 });
