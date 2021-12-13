aeb466aeb49843b40571233677d29d78d26b7bb8
johnjrobert <78804682+johnjrobert@users.noreply.github.com>
2021-12-12 23:11:58 -0500

    Changed to add

--- public/app/app.js
+++ public/app/app.js
@@ -1,3 +1,80 @@
+function initFirebase() {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      firebase
+        .auth()
+        .currentUser.updateProfile({})
+        .then(() => {
+          UpdateSiteWithInfo();
+        });
+      $(".pName").css("display", "block");
+      $(".pName").html(user.fName);
+      loadUserRecipe();
+    } else {
+      loadPublicRecipes();
+      console.log("user is not there");
+      $(".pName").css("display", "none");
+      _db = "";
+    }
+  });
+}
+
+function createUser() {
+  let password = "password"; //$("#password").val();
+  let email = "johnjrobert@gmail.com";
+  let firstName = "John";
+  let lastName = "Robert";
+  fullName = firstName + " " + lastName;
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+      // ..
+    });
+}
+
+function loginUser() {
+  let email = "johnjrobert@gmail.com";
+  let password = "password";
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      console.log("Logged in!");
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+}
+
+function signoutUser() {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      // Sign-out successful.
+      console.log("Signed out");
+    })
+    .catch((error) => {
+      // An error happened.
+      console.log(error);
+    });
+}
+
 function route() {
   let hashTag = window.location.hash;
   let pageID = hashTag.replace("#/", "");
@@ -15,6 +92,7 @@ function route() {
 function initListeners() {
   route();
   $(window).on("hashchange", route);
+  addToCart();
 }
 
 function loop() {
@@ -22,11 +100,17 @@ function loop() {
     $.each(coffee.COFFEEMAKERS, function (index, coffee) {
       console.log("Recipe:" + coffee.name);
       $("#coffeecont").append(
-        `<div id="insideCoffeeCont"><div class="coffee-image"><img src="${coffee.image}" alt=""></div><div class="coffee-info"><h1 class="coffee-name">${coffee.name}</h1><h1 class="coffee-price">Price:  ${coffee.price}</h1><div class="coffee-shipping-icon"><img src="${coffee.shippingicon}"></div></div></div>`
+        `<div id="insideCoffeeCont"><div class="coffee-image"><img src="${coffee.image}" alt=""></div><div class="coffee-info"><h1 class="coffee-name">${coffee.name}</h1><h1 class="coffee-price">Price:  ${coffee.price}</h1><div class="coffee-shipping-icon"><img src="${coffee.shippingicon}"></div><button class="addCart">Add to Cart</button></div></div>`
       );
     });
   }).fail((error) => {
     console.log(error);
+  });
+}
+
+function addToCart() {
+  $(".addCart").on("click", () => {
+    console.log("Hello");
   });
 }
 
